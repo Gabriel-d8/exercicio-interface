7f843504d3c2cd8b7876b7d3f2fdd66fae8d1dc4
Gabriel D'Andréa <webgabrielaraujo@gmail.com>
2023-02-22 16:41:23 -0300

    Criada a base do projeto de locação veicular

---
+++ src/application/Program.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.CarRental;
+import model.entities.Vehicle;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		
+		System.out.println("Enter rental details:");
+		System.out.print("Car model: ");
+		String carModel = sc.nextLine();
+		System.out.println("Date of withdrawal (dd/mm/yyyy hh:mm): ");
+		LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
+		System.out.println("Return date (dd/mm/yyyy hh:mm): ");
+		LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
+		
+		CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
+		
+	}
+
+}
