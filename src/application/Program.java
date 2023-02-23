1f7ec0a537503be98dfea68225af5f395008e541
Gabriel D'Andréa <webgabrielaraujo@gmail.com>
2023-02-23 13:54:43 -0300

    Lógica implementada sem utilizar Interface

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,6 +7,8 @@ import java.util.Scanner;
 
 import model.entities.CarRental;
 import model.entities.Vehicle;
+import model.services.BrazilTaxService;
+import model.services.RentalService;
 
 public class Program {
 
@@ -25,7 +27,21 @@ public class Program {
 		System.out.println("Return date (dd/mm/yyyy hh:mm): ");
 		LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
 		
-		CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
+		CarRental cr = new CarRental(start, finish, new Vehicle(carModel)); 
+		
+		System.out.print("Enter price per hour: ");
+		double pricePerHour = sc.nextDouble();
+		System.out.print("Enter price per day: ");
+		double pricePerDay = sc.nextDouble();
+		
+		RentalService rentalService = new RentalService(pricePerDay, pricePerHour, new BrazilTaxService());
+		
+		rentalService.processInvoice(cr);
+		
+		System.out.println("Invoice: ");
+		System.out.println("Basic payment: " + String.format("%.2f", cr.getInvoice().getBasicPayment()));
+		System.out.println("Tax Service: " + String.format("%.2f", cr.getInvoice().getTax()));
+		System.out.println("Total payment: " + String.format("%.2f", cr.getInvoice().getTotalPayment()));
 		
 	}
 
